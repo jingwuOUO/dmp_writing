be029ab25f093d214bae8348c80718ebe166aa89
jingwuOUO <wujing2951@gmail.com>
2020-02-25 21:25:35 -0500

    generate data for Fetch

--- code/generate.py
+++ code/generate.py
@@ -11,8 +11,11 @@ def generate_data(dmp_traj, name):
     # np.savetxt('%s.txt' % name, (dmp_traj[0, :], dmp_traj[1, :], dmp_traj[2, :]), delimiter='\'')
     with open('%s.txt' % name, 'w') as f:
         for i in range(dmp_traj.shape[1]):
-            x, y, z = 10 * (dmp_traj[:, i] +5.0)
-            line = '[{0}, {1}, {2}]'.format(x, y, z)
+            # x, y, z = 1000 * (dmp_traj[:, i] ) + 80.0
+            y = 18.0
+            x = 600 * (- dmp_traj[0, i]) - 270
+            z = 600 * dmp_traj[2, i] - 90
+            line = '[{0}, {1}, {2}]'.format(x, z, y)
             f.write(line+"\'"+"\n")
 
     print("----------------------Finished saving trajectory file---------------------------------------")
